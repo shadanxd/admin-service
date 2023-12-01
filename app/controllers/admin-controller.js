20efa54fd0888b75f3003e559fbece4c3fe5154d
Shadan K <shadankalam@gmail.com>
2023-12-01 13:27:44 +0530

    added routes

---
+++ app/controllers/admin-controller.js
@@ -0,0 +1,25 @@
+const Model = require('../models/admin-model.js');
+
+// adding new restauarant in the db
+exports.addNew = (req, res) => {
+
+};
+
+// fetching all the restaurants 
+exports.findAll = (req, res) =>{
+
+};
+
+// finding one restaurant 
+exports.findOne = (req, res) =>{
+
+};
+
+// updating details of one restaurant
+exports.update = (req, res) =>{
+
+};
+
+exports.delete = (req, res) =>{
+
+};
--- index.js
+++ index.js
@@ -3,16 +3,10 @@ const app = express();
 const PORT = 3001;
 
 app.get('/', (req, res) =>{
-    res.send('Welcome to Admin Srvice')
+    res.send('Welcome to Admin Service')
 });
 
-app.post('/restaurant/add', (req, res)=> {
-
-})
-
-app.get('/restaurant/feth', (req, res) =>{
-    
-})
+require("./app/routes/admin-routes.routes.js")(app);
 
 app.listen(PORT, () =>{
     console.log(`Node running on port ${PORT}`)
